527f2a068571c8040215460bc8f34d94e3b1f31b
Alex <asxasx>
2018-11-12 15:26:47 +0300

    fix add template

--- LanStatusCheck/Components/TimerIndicator.xaml.cs
+++ LanStatusCheck/Components/TimerIndicator.xaml.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -32,20 +30,6 @@ namespace LanStatusCheck.Components
 
         #region Dep Property local
 
-        //  private static readonly DependencyPropertyKey ArrayStateSegmentsPropertyKey
-        //= DependencyProperty.RegisterReadOnly("ArrayStateSegments", typeof(bool[]), typeof(TimerIndicator),
-        //    new FrameworkPropertyMetadata(new bool[18],
-        //        FrameworkPropertyMetadataOptions.None));
-
-        //  public static readonly DependencyProperty ArrayStateSegmentsProperty
-        //      = ArrayStateSegmentsPropertyKey.DependencyProperty;
-
-        //  public bool[] ArrayStateSegments
-        //  {
-        //      get { return (bool[])GetValue(ArrayStateSegmentsProperty); }
-        //      protected set { SetValue(ArrayStateSegmentsPropertyKey, value); }
-        //  }
-
         private static readonly DependencyPropertyKey ArrayStateSegmentsPropertyKey
       = DependencyProperty.RegisterReadOnly("ArrayStateSegments", typeof(ActiveSegment[]), typeof(TimerIndicator),
           new FrameworkPropertyMetadata(new ActiveSegment[18],
@@ -56,7 +40,7 @@ namespace LanStatusCheck.Components
 
         public ActiveSegment[] ArrayStateSegments
         {
-            get { return (ActiveSegment[])GetValue(ArrayStateSegmentsProperty); }
+            get { return ( ActiveSegment[] ) GetValue(ArrayStateSegmentsProperty); }
             protected set { SetValue(ArrayStateSegmentsPropertyKey, value); }
         }
 
@@ -72,7 +56,7 @@ namespace LanStatusCheck.Components
 
         public Brush ActiveBrush
         {
-            get { return (Brush)GetValue(ActiveBrushProperty); }
+            get { return ( Brush ) GetValue(ActiveBrushProperty); }
             protected set { SetValue(ActiveBrushPropertyKey, value); }
         }
 
@@ -87,7 +71,7 @@ namespace LanStatusCheck.Components
 
         public Brush InactiveBrush
         {
-            get { return (Brush)GetValue(InactiveBrushProperty); }
+            get { return ( Brush ) GetValue(InactiveBrushProperty); }
             protected set { SetValue(InactiveBrushPropertyKey, value); }
         }
 
@@ -106,7 +90,7 @@ namespace LanStatusCheck.Components
 
         public TimeSpan CurrentTimerValue
         {
-            get { return (TimeSpan)GetValue(CurrentTimerValueProperty); }
+            get { return ( TimeSpan ) GetValue(CurrentTimerValueProperty); }
             protected set { SetValue(CurrentTimerValuePropertyKey, value); }
         }
 
@@ -121,7 +105,7 @@ namespace LanStatusCheck.Components
         /// <summary>Цвет заполнения активных индикатров </summary>
         public Color IndicatorActiveColor
         {
-            get { return (Color)GetValue(IndicatorActiveColorProperty); }
+            get { return ( Color ) GetValue(IndicatorActiveColorProperty); }
             set { SetValue(IndicatorActiveColorProperty, value); }
         }
 
@@ -130,9 +114,9 @@ namespace LanStatusCheck.Components
 
         private static void ChangeIndicatorActiveColor(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var control = (TimerIndicator)d;
+            var control = ( TimerIndicator ) d;
 
-            control.ActiveBrush = new SolidColorBrush((Color)e.NewValue);
+            control.ActiveBrush = new SolidColorBrush(( Color ) e.NewValue);
 
         }
 
@@ -140,7 +124,7 @@ namespace LanStatusCheck.Components
         /// <summary>Цвет заполнения неактивных индикатров</summary>
         public Color IndicatorInactiveColor
         {
-            get { return (Color)GetValue(IndicatorInactiveColorProperty); }
+            get { return ( Color ) GetValue(IndicatorInactiveColorProperty); }
             set { SetValue(IndicatorInactiveColorProperty, value); }
         }
 
@@ -149,19 +133,33 @@ namespace LanStatusCheck.Components
 
         private static void ChangeIndicatorInactiveColor(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var control = (TimerIndicator)d;
+            var control = ( TimerIndicator ) d;
 
-            control.InactiveBrush = new SolidColorBrush((Color)e.NewValue);
+            control.InactiveBrush = new SolidColorBrush(( Color ) e.NewValue);
         }
 
 
         #endregion
 
+        #region TypeViewIndicator
+
+        public TypeViewDataIndicator TypeViewDataIndicator
+        {
+            get { return ( TypeViewDataIndicator ) GetValue(TypeViewDataIndicatorProperty); }
+            set { SetValue(TypeViewDataIndicatorProperty, value); }
+        }
+
+        public static readonly DependencyProperty TypeViewDataIndicatorProperty =
+            DependencyProperty.Register("TypeViewDataIndicator", typeof(TypeViewDataIndicator), typeof(TimerIndicator), new PropertyMetadata(0));
+
+        #endregion
+
+
         #region Times
 
         public TimeSpan Duration
         {
-            get { return (TimeSpan)GetValue(DurationProperty); }
+            get { return ( TimeSpan ) GetValue(DurationProperty); }
             set { SetValue(DurationProperty, value); }
         }
 
@@ -170,19 +168,40 @@ namespace LanStatusCheck.Components
 
         private static bool DutationValidate(object value)
         {
-            var val = (TimeSpan)value;
+            var val = ( TimeSpan ) value;
 
             return val.TotalMinutes <= 99;
         }
 
         private static void DurationPropChangeCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var control = (TimerIndicator)d;
+            var control = ( TimerIndicator ) d;
 
             control._timer.Stop();
 
-            control.CurrentTimerValue = (TimeSpan)e.NewValue;
+            control.CurrentTimerValue = ( TimeSpan ) e.NewValue;
         }
+
+
+
+        public double PercentValue
+        {
+            get { return ( double ) GetValue(PercentValueProperty); }
+            set { SetValue(PercentValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Percent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PercentValueProperty =
+            DependencyProperty.Register("PercentValue", typeof(double), typeof(TimerIndicator), new PropertyMetadata(0.0), PercentValueValidate);
+
+        private static bool PercentValueValidate(object value)
+        {
+            var val = ( double ) value;
+
+            return ( ( val >= 0 ) && ( val <= 100 ) );
+        }
+
+
 
         #endregion
 
@@ -236,7 +255,9 @@ namespace LanStatusCheck.Components
 
             }
             else
+            {
                 Stop();
+            }
         }
 
         //private bool[] GetNewArray(TimeSpan currentValue, TimeSpan duration, int countElem)
@@ -261,7 +282,10 @@ namespace LanStatusCheck.Components
 
         private ActiveSegment[] GetNewArray(TimeSpan currentValue, TimeSpan duration, int countElem)
         {
-            if (countElem == 0) throw new ArgumentException("Число эллементов не может быть равно 0");
+            if (countElem == 0)
+            {
+                throw new ArgumentException("Число эллементов не может быть равно 0");
+            }
 
             ActiveSegment[] resArray = new ActiveSegment[countElem];
 
@@ -274,18 +298,18 @@ namespace LanStatusCheck.Components
 
             var valueOneElem = 100.0 / countElem;
 
-            var percent = ((int)currentValue.TotalSeconds * 100.0) / (int)duration.TotalSeconds;
+            var percent = ( ( int ) currentValue.TotalSeconds * 100.0 ) / ( int ) duration.TotalSeconds;
 
             var countFullElem = percent / valueOneElem;
 
-            for (int i = 0; i < (int)countFullElem; i++)
+            for (int i = 0; i < ( int ) countFullElem; i++)
             {
                 resArray[i] = new ActiveSegment() { Value = 1 };
             }
 
-            if (countFullElem - (int)countFullElem != 0)
+            if (countFullElem - ( int ) countFullElem != 0)
             {
-                resArray[(int)countFullElem].Value = countFullElem - (int)countFullElem;
+                resArray[( int ) countFullElem].Value = countFullElem - ( int ) countFullElem;
             }
 
             return resArray;
@@ -295,14 +319,20 @@ namespace LanStatusCheck.Components
 
         public void Stop()
         {
-            if (_timer == null) return;
+            if (_timer == null)
+            {
+                return;
+            }
 
             _timer.Stop();
         }
 
         public void RunTimer()
         {
-            if (_timer == null) return;
+            if (_timer == null)
+            {
+                return;
+            }
 
             if (Duration == TimeSpan.MinValue)
             {
@@ -314,7 +344,10 @@ namespace LanStatusCheck.Components
 
         public void Reset()
         {
-            if (_timer == null) return;
+            if (_timer == null)
+            {
+                return;
+            }
 
             _timer.Stop();
 
@@ -338,7 +371,7 @@ namespace LanStatusCheck.Components
 
         public Color ColorOff
         {
-            get { return (Color)GetValue(ColorOffProperty); }
+            get { return ( Color ) GetValue(ColorOffProperty); }
             set { SetValue(ColorOffProperty, value); }
         }
 
@@ -347,34 +380,33 @@ namespace LanStatusCheck.Components
 
         public Color ColorOn
         {
-            get { return (Color)GetValue(ColorOnProperty); }
+            get { return ( Color ) GetValue(ColorOnProperty); }
             set { SetValue(ColorOnProperty, value); }
         }
 
         public static readonly DependencyProperty ColorOnProperty =
             DependencyProperty.Register("ColorOn", typeof(Color), typeof(BoolToBrushConverter));
 
-        private Dictionary<string, Point[]> _paramGradientForSegment = new Dictionary<string, Point[]>
+        private readonly Dictionary<string, Point[]> _paramGradientForSegment = new Dictionary<string, Point[]>
         {
-            {"1", new Point[]{  new Point(0.028, 0.363),    new Point(0.968, 0.636)     } },
-            {"2", new Point[]{  new Point(0.065, 0.175),    new Point(0.939, 0.819)     } },
-            {"3", new Point[]{ new Point(0.13, 0.104),      new Point(0.863, 0.905),         } },
-            {"4", new Point[]{ new Point(0.226, 0.058),     new Point(0.763, 0.95),          } },
-            {"5", new Point[]{ new Point(0.5, 0.0),         new Point(0.5, 1),               } },
-            {"6", new Point[]{ new Point(0.726, 0.036),     new Point(0.251, 0.952),         } },
-            {"7", new Point[]{ new Point(0.854, 0.083),     new Point(0.121, 0.893),         } },
-            {"8", new Point[]{ new Point(0.94, 0.166),      new Point(0.056, 0.822),         } },
-            {"9", new Point[]{ new Point(0.985, 0.35),  new Point(0.013, 0.631),         } },
-            {"10", new Point[]{new Point(0.992, 0.653), new Point(0.005, 0.364),         } },
-            {"11", new Point[]{new Point(0.939, 0.821), new Point(0.065, 0.173),         } },
-            {"12", new Point[]{new Point(0.877, 0.905), new Point(0.123, 0.082),         } },
-            {"13", new Point[]{new Point(0.77, 0.962),  new Point(0.254, 0.026),         } },
-            {"14", new Point[]{new Point(0.5, 1),       new Point(0.5, 0),               } },
-            {"15", new Point[]{new Point(0.246, 0.973), new Point(0.734, 0.033),         } },
-            {"16", new Point[]{new Point(0.115, 0.909), new Point(0.891, 0.092),         } },
-            {"17", new Point[]{new Point(0.05, 0.803),  new Point(0.944, 0.169),         } },
-            {"18", new Point[]{ new Point(0.03, 0.641),  new Point(0.979, 0.360),         } }
-
+            {"1", new Point[]{new Point(0.028, 0.363), new Point(0.968, 0.636)} },
+            {"2", new Point[]{new Point(0.065, 0.175), new Point(0.939, 0.819)} },
+            {"3", new Point[]{new Point(0.13, 0.104), new Point(0.863, 0.905)} },
+            {"4", new Point[]{new Point(0.226, 0.058), new Point(0.763, 0.95)} },
+            {"5", new Point[]{new Point(0.5, 0.0), new Point(0.5, 1)} },
+            {"6", new Point[]{new Point(0.726, 0.036), new Point(0.251, 0.952)} },
+            {"7", new Point[]{new Point(0.854, 0.083), new Point(0.121, 0.893)} },
+            {"8", new Point[]{new Point(0.94, 0.166), new Point(0.056, 0.822)} },
+            {"9", new Point[]{new Point(0.985, 0.35), new Point(0.013, 0.631)} },
+            {"10", new Point[]{new Point(0.992, 0.653), new Point(0.005, 0.364)} },
+            {"11", new Point[]{new Point(0.939, 0.821), new Point(0.065, 0.173)} },
+            {"12", new Point[]{new Point(0.877, 0.905), new Point(0.123, 0.082)} },
+            {"13", new Point[]{new Point(0.77, 0.962), new Point(0.254, 0.026)} },
+            {"14", new Point[]{new Point(0.5, 1), new Point(0.5, 0)} },
+            {"15", new Point[]{new Point(0.246, 0.973), new Point(0.734, 0.033)} },
+            {"16", new Point[]{new Point(0.115, 0.909), new Point(0.891, 0.092)} },
+            {"17", new Point[]{new Point(0.05, 0.803), new Point(0.944, 0.169)} },
+            {"18", new Point[]{ new Point(0.03, 0.641), new Point(0.979, 0.360)} }
         };
 
 
@@ -385,15 +417,24 @@ namespace LanStatusCheck.Components
             //var val = (bool)value;
 
 
-            if (!(value is ActiveSegment)) return Binding.DoNothing;
+            if (!( value is ActiveSegment ))
+            {
+                return Binding.DoNothing;
+            }
 
-            var val = (ActiveSegment)value;
+            var val = ( ActiveSegment ) value;
 
-            if (val.Value == 0) return new SolidColorBrush(ColorOff);
+            if (val.Value == 0)
+            {
+                return new SolidColorBrush(ColorOff);
+            }
 
-            if (val.Value == 1) return new SolidColorBrush(ColorOn);
+            if (val.Value == 1)
+            {
+                return new SolidColorBrush(ColorOn);
+            }
 
-            var numberSegment = (string)parameter;
+            var numberSegment = ( string ) parameter;
 
             var points = _paramGradientForSegment[numberSegment];
 
@@ -419,12 +460,14 @@ namespace LanStatusCheck.Components
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (!(value is TimeSpan)) return Binding.DoNothing;
+            if (!( value is TimeSpan ))
+            {
+                return Binding.DoNothing;
+            }
 
-            var val = (TimeSpan)value;
+            var val = ( TimeSpan ) value;
 
-            return string.Format("{0:D2}:{1:D2}", (int)val.TotalMinutes, val.Seconds);
-
+            return string.Format("{0:D2}:{1:D2}", ( int ) val.TotalMinutes, val.Seconds);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -444,5 +487,12 @@ namespace LanStatusCheck.Components
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
+
+    public enum TypeViewDataIndicator
+    {
+        Timer,
+        Percent
+    }
+
 
 }
